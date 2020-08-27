e9a3adfab108432eedca96f656c0a5531985609e
zxr19940826 <1270683737@qq.com>
2020-08-27 19:40:31 +0800

    配置了项目中的vant

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 导入通用的样式
+import './styles/base.less'
+import './styles/iconfont.css'
+// 导入vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
+// 全局的把vant所有的组件都导入好了
+Vue.use(Vant)
+
+// 关闭控制台的提示消息
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,14 @@
 <template>
-  <dir>登录组件</dir>
+  <dir>
+    登录组件
+    <span class="iconfont icondianzan"></span>
+    <span class="iconfont iconweixin"></span>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+  </dir>
 </template>
 
 <script>
